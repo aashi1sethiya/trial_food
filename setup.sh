af6586619edef4ac3367cbc04805491b4d6a098a
I Kit Cheng <matthewkit@gmail.com>
2022-02-15 10:19:52 +0800

    [UPDATE] Add streamlit theme into config.toml from setup file

--- setup.sh
+++ setup.sh
@@ -5,9 +5,13 @@ echo "\
 email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo "[theme]
+primaryColor='#020202'
+backgroundColor='#c4c3c3'
+secondaryBackgroundColor='#ebd316'
+font = 'sans serif'
+[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
